acaf11c32db1a2923071df97a73d47740e503858
Henrique <henrynyyls@gmail.com>
2024-01-30 20:52:03 -0300

    Starting to work on the JS

---
+++ script.js
@@ -0,0 +1,32 @@
+const verdeCipreste = {
+  nome: 'Verde-cipestre',
+  pasta: 'imagens-verde-cipreste'
+};
+
+const azulInverno = {
+  nome: "Azul-Inverno",
+  pasta: "imagens-azul-inverno"
+};
+
+const meiaNoite = {
+  nome: "Meia-Noite",
+  pasta: "imagens-meia-noite"
+};
+
+const estelar = {
+  nome: "Estelar",
+  pasta: 'imagens-estelar'
+};
+
+const rosaClaro = {
+  nome: "Rosa-Claro",
+  pasta: 'imagens-rosa-claro'
+};
+
+const opcoesCores = [verdeCipreste,azulInverno,meiaNoite,estelar,rosaClaro];
+
+let imagemSelecionada = 1;
+
+function trocarImage(){
+  const opcaoSelecionada = document.querySelector('[name:"opcao-imagem"]:checked')
+}
