928dadca4bc2fc81bc38b5e479dc4c0e349400bb
Henrique <henrynyyls@gmail.com>
2024-01-30 21:02:01 -0300

    Just did some changes on the JS

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const imagemVisualizacao = document.getElementById('imagem-visualizacao');
+
 const verdeCipreste = {
   nome: 'Verde-cipestre',
   pasta: 'imagens-verde-cipreste'
@@ -28,5 +30,7 @@ const opcoesCores = [verdeCipreste,azulInverno,meiaNoite,estelar,rosaClaro];
 let imagemSelecionada = 1;
 
 function trocarImage(){
-  const opcaoSelecionada = document.querySelector('[name:"opcao-imagem"]:checked')
+  const idOpcaoSelecionada = document.querySelector('[name:"opcao-imagem"]:checked');
+  imagemSelecionada = idOpcaoSelecionada.charAt(0);
+  imagemVisualizacao.src = './imagens/opcoes-cores/imagens-azul-inverno/imagem-' + imagemSelecionada + '.jpeg';
 }
