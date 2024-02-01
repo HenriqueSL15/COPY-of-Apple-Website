81a201913b3f8331f98594b5f1fbf5efcaab6d90
Henrique <henrynyyls@gmail.com>
2024-02-01 09:47:59 -0300

    Added documentation for easier reading of the code

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const minuaturaImagem0 = document.getElementById('0-imagem-miniatura');
 const minuaturaImagem1 = document.getElementById('1-imagem-miniatura');
 const minuaturaImagem2 = document.getElementById('2-imagem-miniatura');
 
+// Objetos com todas as cores(e seus respectivos nomes/pastas)
 const azulInverno = {
   nome: 'Azul-inverno',
   pasta: 'imagens-azul-inverno'
@@ -30,23 +31,35 @@ const rosaClaro = {
   pasta: 'imagens-rosa-claro'
 };
 
+// Array guardando todos os objetos
 const opcoesCores = [verdeCipreste,azulInverno,meiaNoite,estelar,rosaClaro];
+
+// Array guardando todos os tamanhos
 const opcoesTamanhos = ['41 mm', '45 mm'];
 
 let imagemSelecionada = 1;
 let tamanhoSelecionado = 1;
 let corSelecionada = 1;
 
+// Função que troca de imagem
 function trocarImagem(){
-  const idOpcaoSelecionada = document.querySelector('[name="opcao-imagem"]:checked').id.charAt(0);;
+  // Pega o primeiro caractere do ID da opção com NAME = "opcao-imagem" que estiver selecionada(checked)
+  const idOpcaoSelecionada = document.querySelector('[name="opcao-imagem"]:checked').id.charAt(0);
+
+  // Atualiza na variável de imagem selecionada
   imagemSelecionada = idOpcaoSelecionada;
+
+  // Troca a visualização com a variável acima além de escolher a pasta corretamente usando outro array
   imagemVisualizacao.src=  "./imagens/opcoes-cores/"+opcoesCores[corSelecionada].pasta + '/imagem-'+ imagemSelecionada + '.jpeg';
 }
 
+// Troca tamanho com base na opção com NAME="opcao-tamanho" que estiver selecionada(checked)
 function trocarTamanho(){
   const idOpcaoSelecionada = document.querySelector('[name="opcao-tamanho"]:checked').id.charAt(0);
   tamanhoSelecionado = idOpcaoSelecionada;
   tituloProduto.innerText = 'Pulseira loop esportiva '+ opcoesCores[corSelecionada].nome +' para caixa de ' + opcoesTamanhos[tamanhoSelecionado];
+
+  // Altera os tamanhos com base no selecionada e vice-versa
   if(opcoesTamanhos[tamanhoSelecionado] === '41 mm'){
     imagemVisualizacao.classList.add('caixa-pequena');
   }else{
@@ -54,14 +67,21 @@ function trocarTamanho(){
   }
 }
 
+
+// Troca a cor com base na opção selecionada
 function trocarCor(){
   const idOpcaoSelecionada = document.querySelector('[name="opcao-cor"]:checked').id.charAt(0);
   corSelecionada = idOpcaoSelecionada;
+  
+  // Altera texto informativo
   tituloProduto.innerText = 'Pulseira loop esportiva '+ opcoesCores[corSelecionada].nome +' para caixa de ' + opcoesTamanhos[tamanhoSelecionado];
   nomeCor.innerText = 'Cor - ' + opcoesCores[corSelecionada].nome;
+
+  // Atualiza as 3 fotos com base na cor selecionada e suas respectivas pastas
   minuaturaImagem0.src = "./imagens/opcoes-cores/"+opcoesCores[corSelecionada].pasta + '/imagem-0.jpeg';
   minuaturaImagem1.src = "./imagens/opcoes-cores/"+opcoesCores[corSelecionada].pasta + '/imagem-1.jpeg';
   minuaturaImagem2.src = "./imagens/opcoes-cores/"+opcoesCores[corSelecionada].pasta + '/imagem-2.jpeg';
 
+  // Altera a visualização da foto atual
   imagemVisualizacao.src=  "./imagens/opcoes-cores/"+opcoesCores[corSelecionada].pasta + '/imagem-'+ imagemSelecionada + '.jpeg';
 }
